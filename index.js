12c929d216076ffde7d13a814b9d3d7751b01d8a
hccde <1076663958@qq.com>
2018-01-05 01:51:29 +0800

    mkdir async

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let utils = require('./lib/utils');
+let path = require('path');
 let Path = require('./lib/interface/Path');
 let fs = require('fs');
 class File {
@@ -14,19 +15,30 @@ File.copy = function(source,dest){
 
 }
 File.mkdir = async function(_path,mode = 0o777){ // loop itself
-	let pathObj = await utils.pathWrapper(_path);
-	console.log(pathObj instanceof Path)
+	if(typeof _path === "string"){
+		_path = await utils.pathWrapper(_path)	
+	}
+	if(_path instanceof Path){
+		let existPath = await utils.findExistDir(_path);
 
-	//if path exist
-	return new Promise(function(reslove,reject){
-		fs.mkdir(_path,mode,function(err){
-			if(err){
-				reject(`File::mkdir${err.toString()}`);
-			}
-			reslove(_path);
-		})
-	})
+		if(existPath.length == _path.dirSep.length){
+			return _path.absolutePath;
+		}
+		//todo
+		let index = _path.pathList.findIndex(function(e){
+			return e === existPath;
+		});
+		for(let i = index+1;i<_path.pathList.length;i++){
+			fs.mkdirSync(_path.pathList[i],mode);
+		}
+		return _path.absolutePath;
+
+	}else{
+		await true;
+		throw Error(`first param must be instance of Path`)
+	}
 }
+//todo
 File.mkdirSync = fs.mkdirSync;
 File.write = function(dest,str){
 	fs.writeFileSync(dest,str);
--- test.js
+++ test.js
@@ -1,9 +1,16 @@
 let File = require('./index');
 let utils = require('./lib/utils');
+let Path = require('./lib/interface/Path');
 let path = require('path');
-File.mkdir('./test').then(function(value){
+File.mkdir('./my/hcc/de/hcc/cong/cong/hao/a.txt').then(function(value){
 	console.log(value)
 })
+// utils.findExistDir(new Path('/Users/admos/Documents/github/tt/a.txt',false)).then(function(value){
+// 	console.log(value)
+// })
+// File.mkdir('./test').then(function(value){
+// 	console.log(value)
+// })
 // utils.pathWrapper('./aa/www').then(function(value){
 // 	console.log(value)
 // });
