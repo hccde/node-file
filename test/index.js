8c6590469527e3ff9affd51c5d87d7f814acd970
hccde <1076663958@qq.com>
2018-01-10 18:28:48 +0800

    test scripts

--- test/index.js
+++ test/index.js
@@ -2,6 +2,7 @@ let File = require('../index');
 let dirTest = require('./mixin/dir');
 let fileTest = require('./mixin/file');
 let helperTest =  require('./mixin/helper');
-// dirTest();
-// fileTest();
+
+dirTest();
+fileTest();
 helperTest();
