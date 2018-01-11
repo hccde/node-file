b0147455e732a62fd9f412b7333b3f5e91c951bd
hccde <1076663958@qq.com>
2018-01-11 15:46:32 +0800

    writeStram && readStream

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ let dir = require('./mixin/dir');
 let file = require('./mixin/file');
 let helper = require('./mixin/helper');
 let url = require('./mixin/url');
+let ReadStream = require('./lib/interface/ReadStream');
+let WriteStream = require('./lib/interface/WriteStream');
+
 mixin(File,dir);
 mixin(File,file);
 mixin(File,helper);
 mixin(File,url);
+File.ReadStream = ReadStream;
+File.WriteStream = WriteStream;
 module.exports = File;
--- test.js
+++ test.js
@@ -3,49 +3,5 @@ let utils = require('./lib/utils');
 let Path = require('./lib/interface/Path');
 let path = require('path');
 let fs = require('fs');
-// File.delete('./my/').then(function(value){
-// 	console.log(value)
-// });
-console.log(File.test())
-// File.merge('./my/','./my2/')
-// files.move('./my/','./my2/')
-// File.copy('./my/a.js','./my2/')
-// File.flat('./lib/').then(function(value){
-// 	console.log(value)
-// });
-// console.log(File.llSync('./test/'));
-// console.log(File.rmdirSync('./softlink/'))
-// console.log(File.bfsSync('./my/'))
-// fs.readdir('./my',function(err,list){
-	// if(err)
-		// console.log(err)
-	// console.log(fs.lstatSync('./my/softlink').isSymbolicLink())
-	// list.forEach((e)=>{
-	// 	console.log(e);
-	// 	console.log(fs.lstatSync(e).isDirectory())
-	// })
-// })
-// File.rmdir('./my/files/a.js').then(function(value){
-// 	console.log(value)
-// })
-// File.readFile('./my/file/a.js').then(function(value){
-// 	console.log(value.toString())
-// })
-// File.writeFileSync('./my/file/a.js','aa');
-// File.writeFile('./my/file/a.js','aa').then(function(value){
-// 	console.log(value)
-// })
-// File.writeSync('./my/softlink/a.js','44444')
-// File.mkdir('./my/hcc/cong/de/').then(function(value){
-// 	console.log(value)
-// })
-// utils.findExistDir(new Path('./my/softlink/a.js',true)).then(function(value){
-	// console.log(value)
-// })
-// File.mkdir('./test').then(function(value){
-// 	console.log(value)
-// })
-// utils.pathWrapper('./aa/www').then(function(value){
-// 	console.log(value)
-// });
-// console.log(File.copy('./index/js','./test/index.js'))
+let stream = new File.WriteStream('./util.js');
+console.log(global.streamList);
