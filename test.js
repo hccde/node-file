d5fb3316c057405bc398cd6a86be06fc6320e0c2
hccde <1076663958@qq.com>
2018-01-11 17:34:26 +0800

    error

--- test.js
+++ test.js
@@ -3,5 +3,9 @@ let utils = require('./lib/utils');
 let Path = require('./lib/interface/Path');
 let path = require('path');
 let fs = require('fs');
-let stream = new File.WriteStream('./util.js');
+let stream = new File.ReadStream('./util.js');
+stream.on('data',function(){
+	console.log(1)
+});
+stream.read();
 console.log(global.streamList);
